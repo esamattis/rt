f969d364c9efdbea6dacafd100fbf8464d9aaa48
Esa-Matti Suuronen <esa-matti@suuronen.org>
2024-09-09 16:41:16 +0300

    Handle export and quotes in .rtenv

--- src/envfile.rs
+++ src/envfile.rs
@@ -12,6 +12,17 @@ impl EnvFile {
         for line in lines {
             let parts = line.split_once('=');
             if let Some((key, value)) = parts {
+                let key = key.trim().trim_start_matches("export ");
+                let value = value.trim();
+
+                let value = if (value.starts_with('\'') && value.ends_with('\''))
+                    || (value.starts_with('"') && value.ends_with('"'))
+                {
+                    &value[1..value.len() - 1]
+                } else {
+                    value
+                };
+
                 vars.insert(key.to_string(), value.to_string());
             }
         }
@@ -40,5 +51,21 @@ mod tests {
         assert_eq!(env_file.get("KEY1"), Some("value1"));
         assert_eq!(env_file.get("KEY2"), Some("value2"));
         assert_eq!(env_file.get("NONEXISTENT"), None);
+    }
+
+    #[test]
+    fn test_ignores_exports() {
+        let env_file = EnvFile::new("export KEY1=value1\nexport KEY2=value2");
+
+        assert_eq!(env_file.get("KEY1"), Some("value1"));
+        assert_eq!(env_file.get("KEY2"), Some("value2"));
+    }
+
+    #[test]
+    fn test_handles_quotes() {
+        let env_file = EnvFile::new("KEY1=\"value1 foo\"\nKEY2='value2 bar'");
+
+        assert_eq!(env_file.get("KEY1"), Some("value1 foo"));
+        assert_eq!(env_file.get("KEY2"), Some("value2 bar"));
     }
 }
